4d0b9605f9153ed1d1993fccf01a7fba9f0aca1d
Bartosz Grabek <bartoszgrabek7@gmail.com>
2019-08-08 14:18:09 +0200

    transfered not-GUI related functions to another file

--- cracker v.1.8.0. new.py
+++ cracker v.1.8.0. new.py
@@ -9,6 +9,7 @@ from PyQt5.QtWidgets import QTableWidget,QTableWidgetItem
 from datetime import datetime, timedelta
 from math import sqrt
 from collections import Counter, OrderedDict
+from math_help import divisors, GCD, find_distance, transformer
 #########################################CONSTANTS############################################
 version = "1.7.7"
 language = "English" # option???
@@ -628,73 +629,6 @@ class Ui_MainWindow(object):
         self.label_fs.setText("Font size: "+str(size))
 
 #################################################FUNCTIONS############################################
-def divisors(n):
-    """
-
-    :param n: number n
-    :return: list of divisors of n
-    """
-    divs = {1,n}
-    for i in range(2,int(sqrt(n))+1):
-        if n%i == 0:
-            divs.update((i,n//i))
-    return sorted(list(divs))
-
-def GCD(list_1):
-    """
-
-    :param list_1: list of numbers
-    :return: greatest common divisor gcd of numbers in the list_1
-    """
-    numbers_sorted = sorted(list_1)
-    gcd = numbers_sorted[0]
-
-    for i in range(1, int(len(list_1))):
-        divisor = gcd
-        dividend = numbers_sorted[i]
-        remainder = dividend % divisor
-        if remainder == 0:
-            gcd = divisor
-        else:
-            while not remainder == 0:
-                dividend_one = divisor
-                divisor_one = remainder
-                remainder = dividend_one % divisor_one
-                gcd = divisor_one
-
-    return gcd
-
-def find_distance(text, el, num):
-    """
-
-    :param text: text
-    :param el: name of repeating fragment
-    :param num: number of appearings of such fragment
-    :return: the distance between two exact fragments
-    """
-    list_of_distances = []
-    k = 0
-    for i in range(num):
-        try:
-            p = text.index(el, k)
-            q = text.index(el, p + 1)
-            distance = (q - p)                 # earlier: distance = (1-p-(len(el)))
-            list_of_distances.append(distance)
-            k += distance + len(el) - 1
-        except ValueError:
-            continue
-    return list_of_distances
-
-def transformer(list_1):
-    """
-    Function displaces elements of list by 1
-    transformer([1,2,3,4]) --> [4,1,2,3]-->[3,4,1,2]-->[2,3,4,1]
-    :param list_1: list
-    :return: list transformed by 1; last element becomes first and first becomes second
-    """
-    end = list_1.pop()    #erase end and copy it to the beginning
-    list_1.insert(0, end)
-    return list_1
 
 ####################################FILE EXECUTION###################################################
 if __name__ == "__main__":
